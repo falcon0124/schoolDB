e5ad6111f6964c2afb5851fb4ca0f32f3abd29e9
sangam saurav <sangamsh04@gmail.com>
2025-06-19 23:16:59 +0530

    cleaning up

--- db.js
+++ db.js
@@ -1,5 +1,4 @@
-const mysql = require('mysql2/promise'); // Use the promise version
-
+const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
